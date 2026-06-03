c4319b71d3800f7d85c2fb5f1d560e5c3dbc6f8d
johnvincentdeseo <johnvincent.deseo@cvsu.edu.ph>
2026-06-03 21:45:45 +0800

    Update 2026_05_16_142509_add_profile_image_to_users_table.php

--- database/migrations/2026_05_16_142509_add_profile_image_to_users_table.php
+++ database/migrations/2026_05_16_142509_add_profile_image_to_users_table.php
@@ -1,22 +1,17 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $blueprint) {
-            $blueprint->string('profile_image')->nullable()->after('email');
-        });
+        DB::statement('ALTER TABLE users MODIFY profile_picture LONGTEXT NULL');
     }
 
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $blueprint) {
-            $blueprint->dropColumn('profile_image');
-        });
+        DB::statement('ALTER TABLE users MODIFY profile_picture VARCHAR(255) NULL');
     }
 };
